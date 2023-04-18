4a7d8c7297f98817a563ce037877d2dc4aa36438
oussama <osm2014@hotmail.fr>
2023-04-18 01:55:14 +0000

    initialize dof struct

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,8 +1,7 @@
 #include <stdlib.h>
 #include "dog.h"
-
 /**
- * init_dog - initializes a variable of type struct dog
+ * init_dog - init fr the dog struct
  * @d: pointer to struct dog 
  * @name: name 
  * @age: age 
